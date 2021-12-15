1719f5ab89e260c4f96bcb37140ffb0ddaf3741d
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2021-12-15 12:40:43 +0100

    add ctrl and alt to numpad

--- keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
+++ keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_DOWN, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_ASTR, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_NO, KC_RGHT, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_PLUS, KC_NO, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_MINS, KC_NO, KC_NO, KC_NO,
-            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0, KC_0, KC_COMM, KC_DOT, KC_NO, KC_NO, KC_NO, KC_NO),
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LALT, KC_LCTL, KC_0, KC_0, KC_COMM, KC_DOT, KC_NO, KC_NO, KC_NO, KC_NO),
 	[4] = LAYOUT_65_with_macro( // arrows on right
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_NO, KC_LSFT, KC_LSFT, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_UP, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
