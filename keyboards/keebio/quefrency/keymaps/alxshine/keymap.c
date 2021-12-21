e38aa67c812ba7bf61f68e32e1f211fe90ec4058
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2021-12-21 14:37:53 +0100

    make F order consistent with normal numpad (not mirrored)

--- keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
+++ keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	[7] = LAYOUT_65_with_macro( // F keys on left
             KC_AUDIO_MUTE, KC_NO, KC_NO, KC_NO, KC_NO , KC_NO , KC_NO , KC_NO, KC_NO, KC_NO  , KC_NO  , KC_NO, KC_NO  , KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME,
-            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F3 , KC_F2 , KC_F1 , KC_NO, KC_NO, KC_LSFT, KC_LSFT, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F6 , KC_F5 , KC_F4 , KC_NO, KC_NO, KC_LCTL, KC_LCTL, KC_NO, KC_LCTL, KC_NO, KC_NO, KC_NO,
-            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F9 , KC_F8 , KC_F7 , KC_NO, KC_NO, KC_LALT, KC_LALT, KC_NO, KC_LALT, KC_NO, KC_NO, KC_NO,
-            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F12, KC_F11, KC_F10, KC_NO, KC_NO, KC_NO  , KC_NO  , KC_NO, KC_NO  , KC_NO, KC_NO
+            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F1 , KC_F2 ,  KC_F3, KC_NO, KC_NO, KC_LSFT, KC_LSFT, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F4 , KC_F5 , KC_F6 , KC_NO, KC_NO, KC_LCTL, KC_LCTL, KC_NO, KC_LCTL, KC_NO, KC_NO, KC_NO,
+            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F7 , KC_F8 , KC_F9 , KC_NO, KC_NO, KC_LALT, KC_LALT, KC_NO, KC_LALT, KC_NO, KC_NO, KC_NO,
+            KC_NO        , KC_NO, KC_NO, KC_NO, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO  , KC_NO  , KC_NO, KC_NO  , KC_NO, KC_NO
     )
 };
