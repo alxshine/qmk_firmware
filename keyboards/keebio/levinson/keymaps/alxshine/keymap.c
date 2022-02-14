7f39af64893d4f8fbf6e671abaeadd7c9baa5df8
Alexander Schlögl <Alexander Schlgl>
2022-02-14 10:10:11 +0100

    add right curly brace to levinson

--- keyboards/keebio/levinson/keymaps/alxshine/keymap.c
+++ keyboards/keebio/levinson/keymaps/alxshine/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM , KC_AT  , KC_HASH,  KC_DLR, KC_PERC, /* SPLIT */ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL ,  _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, /* SPLIT */ _______, KC_QUES, KC_PLUS, _______, _______, KC_PIPE, \
+  KC_DEL ,  _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, /* SPLIT */ _______, KC_QUES, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, _______ , _______, KC_LBRC, KC_RBRC, _______, /* SPLIT */ _______, _______, _______, KC_HOME,  KC_END, _______, \
   _______, _______ , _______, _______, _______, _______, /* SPLIT */ _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
