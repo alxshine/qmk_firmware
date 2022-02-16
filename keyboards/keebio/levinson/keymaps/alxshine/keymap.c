580b2f85db707dd77b2808c89a08edbac9ca220a
Alexander Schlögl <Alexander Schlgl>
2022-02-16 12:19:46 +0100

    some improvements

--- keyboards/keebio/levinson/keymaps/alxshine/keymap.c
+++ keyboards/keebio/levinson/keymaps/alxshine/keymap.c
@@ -21,10 +21,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_DVORAK] = LAYOUT_ortho_4x12( \
- KC_TAB        , KC_QUOT, KC_COMM, KC_DOT , KC_P      , KC_Y  , /* SPLIT */ KC_F  , KC_G      , KC_C   , KC_R   , KC_L , KC_BSPC, \
- LCTL_T(KC_ESC), KC_A   , KC_O   , LT(_NUMPAD, KC_E), LT(_ARROWS, KC_U), KC_I  , /* SPLIT */ KC_D  , KC_H      , KC_T   , LT(_FUNCS, KC_N)   , KC_S , KC_MINS, \
- KC_LSFT       , KC_SCLN, KC_Q   , KC_J   , KC_K      , KC_X  , /* SPLIT */ KC_B  , KC_M      , KC_W   , KC_V   , KC_Z , KC_ENT , \
- MO(_ADJUST)   , KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, /* SPLIT */ KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+ KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                           /* SPLIT */ KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSPC, \
+ LCTL_T(KC_ESC), KC_A, KC_O, LT(_NUMPAD, KC_E), LT(_ARROWS, KC_U), KC_I, /* SPLIT */ KC_D, KC_H, KC_T, LT(_FUNCS, KC_N), KC_S, KC_MINS, \
+ KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,                               /* SPLIT */ KC_B, KC_M, KC_W, KC_V, KC_Z, KC_ENT, \
+ MO(_ADJUST), KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), LT(_LOWER, KC_SPC),             /* SPLIT */ LT(_RAISE, KC_SPC), MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
 [_NUMPAD] = LAYOUT_ortho_4x12( \
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV ,  KC_1  ,    KC_2,    KC_3,    KC_4 ,    KC_5, /* SPLIT */ KC_6, KC_7  ,    KC_8,    KC_9 ,    KC_0, KC_BSPC, \
-  KC_DEL ,  KC_F1 ,   KC_F2,   KC_F3,   KC_F4 ,   KC_F5, /* SPLIT */ _______, KC_SLSH, KC_EQL ,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F6  ,   KC_F7,   KC_F8,   KC_F9,  KC_F10, /* SPLIT */  _______, _______ , _______, KC_PGUP , KC_PGDN, _______, \
-  _______, _______, _______, _______, KC_F11 , KC_F12, /* SPLIT */   _______, _______  , KC_MNXT, KC_VOLD , KC_VOLU, KC_MPLY \
+  KC_DEL,  _______,   _______,   _______, _______,   _______, /* SPLIT */ _______, KC_SLSH, KC_EQL ,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, _______,   _______,   _______, _______,  _______, /* SPLIT */  _______, _______ , _______, KC_PGUP , KC_PGDN, _______, \
+  _______, _______, _______, _______, _______, _______, /* SPLIT */   _______, _______  , KC_MNXT, KC_VOLD , KC_VOLU, KC_MPLY \
 ),
 
 [_ADJUST] =  LAYOUT_ortho_4x12( \
