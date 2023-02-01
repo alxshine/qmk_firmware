a7da6598085ea8dcbd14da4209ec8bb1ca4fe7cf
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-02-01 23:34:48 +0100

    Add media layer

--- keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
+++ keyboards/keebio/quefrency/keymaps/alxshine/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_with_macro( // dvorak
             KC_MUTE,  KC_MPLY,  /*   */ KC_GRV        , KC_1    , KC_2    , KC_3       , KC_4          , KC_5   , KC_6   , KC_7       , KC_8   , KC_9       , KC_0   , KC_LBRC, KC_RBRC, KC_DEL , KC_BSPC,  /*   */ KC_HOME, 
-            KC_MPRV,  KC_MNXT,  /*   */ KC_TAB        , KC_QUOT , KC_COMM , KC_DOT     , KC_P          , KC_Y   , KC_F   , KC_G       , KC_C   , KC_R       , KC_L   , KC_SLSH, KC_EQL , KC_BSLS,           /*   */ KC_PGUP, 
+            KC_MPRV,  KC_MNXT,  /*   */ KC_TAB        , KC_QUOT , KC_COMM , LT(8, KC_DOT)     , KC_P          , KC_Y   , KC_F   , KC_G       , KC_C   , KC_R       , KC_L   , KC_SLSH, KC_EQL , KC_BSLS,           /*   */ KC_PGUP, 
             _______,  _______,  /*   */ LCTL_T(KC_ESC), KC_A    , KC_O    , LT(3, KC_E), LT(4, KC_U)   , KC_I   , KC_D   , LT(6, KC_H), KC_T   , LT(7, KC_N), KC_S   , KC_MINS, KC_ENT ,                    /*   */ KC_PGDN, 
             KC_CUT ,  KC_COPY,  /*   */ KC_LSFT       , KC_SCLN , KC_Q    , KC_J       , KC_K          , KC_X   , KC_B   , KC_M       , KC_W   , KC_V       , KC_Z   , KC_RSFT, KC_UP  ,                    /*   */ KC_END, 
             _______,  KC_PSTE,  /*   */ MO(2)         , KC_LCTL , KC_LGUI , KC_LALT    , LT(4, KC_BSPC), KC_SPC , KC_SPC , KC_RALT    , KC_RCTL, _______    , KC_LEFT, KC_DOWN,                             /*   */ KC_RGHT
@@ -55,6 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,  _______,  /*   */ _______       , KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______, KC_LCTL, KC_LCTL, _______, KC_LCTL, _______,                          _______, /*   */  _______, 
             _______,  _______,  /*   */ _______       , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_LALT, KC_LALT, _______, KC_LALT, _______,                          _______, /*   */  _______, 
             KC_WH_U,  _______,  /*   */ _______       , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                   _______, /*   */  _______
+    ),
+	[8] = LAYOUT_65_with_macro( // media and other controls
+            KC_MUTE,  _______,  /*   */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, /*   */  KC_HOME, 
+            _______,  _______,  /*   */ _______, _______, _______, _______, _______, _______, _______, _______, KC_F13 , KC_F14 , _______, _______, _______,                 _______, /*   */  _______, 
+            _______,  _______,  /*   */ _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, _______,                          _______, /*   */  _______, 
+            _______,  _______,  /*   */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                          _______, /*   */  _______, 
+            KC_WH_U,  _______,  /*   */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                   _______, /*   */  _______
     )
 };
 
