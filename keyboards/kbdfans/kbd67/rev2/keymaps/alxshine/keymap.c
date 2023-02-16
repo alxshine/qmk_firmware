a14e7d19a84b98ef377f1b4283dff18a60b7ea28
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-02-16 15:21:32 +0100

    make new keyboard usable

--- keyboards/kbdfans/kbd67/rev2/keymaps/alxshine/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/alxshine/keymap.c
@@ -1,45 +1,24 @@
-/* Copyright 2018 'mechmerlin'
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap (Base Layer) Default Layer
-   * ,----------------------------------------------------------------.
-   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
-   * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
-   * |----------------------------------------------------------------|
-   * |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
-   * |----------------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
-   * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt | fn     |win| space    |Alt| FN|Ctrl|Lef|Dow|Rig |
-   * `----------------------------------------------------------------'
-   */
-[0] = LAYOUT_65_ansi_split_space(
-  KC_GRV        , KC_1   , KC_2   , KC_3  , KC_4   , KC_5  , KC_6   , KC_7 , KC_8   , KC_9   , KC_0   , KC_LBRC , KC_RBRC, KC_BSPC, KC_HOME, 
-  KC_TAB        , KC_QUOT, KC_COMM, KC_DOT, KC_P   , KC_Y  , KC_F   , KC_G , KC_C   , KC_R   , KC_L   , KC_SLSH , KC_EQL, KC_BSLS, KC_PGUP, 
-  LCTL_T(KC_ESC), KC_A   , KC_O   , KC_E  , KC_U   , KC_I  , KC_D   , KC_H , KC_T   , KC_N   , KC_S   , KC_MINS , KC_ENT, KC_PGDN, 
-  KC_LSFT       , KC_SCLN, KC_Q   , KC_J  , KC_K   , KC_X  , KC_B   , KC_M , KC_W   , KC_V   , KC_Z   , KC_RSFT , KC_UP , KC_END , 
-  MO(2)         , KC_LGUI, KC_LALT, KC_BSPC , OSL(2), KC_SPC, KC_RALT, MO(2), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
+enum layers {
+    _DVORAK,
+    _QWERTY,
+    _FUNC,
+    _NUM
+};
 
-[1] = LAYOUT_65_ansi_split_space(
-  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC,
-  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-  KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_PGUP,
-  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,           KC_UP, KC_PGDN,
-  MO(2), KC_LGUI, KC_LALT,    MO(1),        KC_LGUI,        KC_SPC,  KC_RALT, MO(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT          ),
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_DVORAK] = LAYOUT_all(
+      KC_GRV        , KC_1          , KC_2   , KC_3    , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC , KC_RBRC , KC_DEL , KC_BSPC, KC_HOME, 
+      KC_TAB        , KC_QUOT       , KC_COMM, KC_DOT  , KC_P   , KC_Y   , KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_SLSH , KC_EQL  , KC_BSLS, KC_PGUP, 
+      LCTL_T(KC_ESC), KC_A          , KC_O   , KC_E    , KC_U   , KC_I   , KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_MINS , KC_ENT  , KC_PGDN, 
+      KC_LSFT       , _______       , KC_SCLN, KC_Q    , KC_J   , KC_K   , KC_X   , KC_B   , KC_M   , KC_W   , KC_V   , KC_Z    , KC_RSFT , KC_UP  , KC_END , 
+      MO(2)         , KC_LGUI       , KC_LALT, KC_BSPC , OSL(2) , KC_SPC , KC_RALT, MO(2)  , KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
+
+    [_QWERTY] = LAYOUT_all(
+        LT(_NUM,KC_ESC),KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC, KC_BSPC, KC_DEL, 
+        KC_TAB,           KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,       KC_PGUP, 
+        LT(_FUNC,KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,             KC_PGDN, 
+        KC_LSFT, KC_NUBS,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,         KC_UP,        MO(_FUNC), 
+        KC_LCTL, KC_LGUI, KC_LALT,        KC_SPC,         KC_SPC,          KC_SPC,          KC_RALT, KC_RCTL, XXXXXXX, KC_LEFT, KC_DOWN,      KC_RGHT),
 };
