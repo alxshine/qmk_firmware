43824654ba5ec4a4a1a48b05064f35a3ee75f9c0
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-04-26 21:00:26 +0200

    add cs go F-keys, grave on braces

--- keyboards/kbdfans/kbd67/rev2/keymaps/alxshine/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/alxshine/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MO(_ADJUST)         , KC_LGUI       , KC_LALT, LGUI_T(KC_BSPC)  , OSL(_ADJUST)     , KC_SPC , KC_RALT, MO(_ADJUST)      , KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
 
     [_QWERTY] = LAYOUT_all(
-      KC_GRV , KC_1   , KC_2   , KC_3  , KC_4        , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS , KC_EQL , KC_BSPC, KC_BSPC, KC_DEL, 
-      KC_TAB , KC_Q   , KC_W   , KC_E  , KC_R        , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC , KC_RBRC, KC_BSLS, KC_PGUP, 
-      KC_LCTL, KC_A   , KC_S   , KC_D  , KC_F        , KC_G  , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT , KC_ENT , KC_PGDN, 
-      KC_LSFT, KC_NUBS, KC_Z   , KC_X  , KC_C        , KC_V  , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH , KC_RSFT, KC_UP  , KC_END , 
+      KC_GRV , KC_1   , KC_2   , KC_3  , KC_4        , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS , KC_EQL , KC_DEL , KC_BSPC, KC_F3, 
+      KC_TAB , KC_Q   , KC_W   , KC_E  , KC_R        , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC , KC_RBRC, KC_BSLS, KC_F1, 
+      KC_LCTL, KC_A   , KC_S   , KC_D  , KC_F        , KC_G  , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT , KC_ENT , KC_F2  ,
+      KC_LSFT, KC_NUBS, KC_Z   , KC_X  , KC_C        , KC_V  , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH , KC_RSFT, KC_UP  , KC_F4, 
       KC_ESC , KC_LGUI, KC_LALT, KC_SPC, OSL(_ADJUST), KC_SPC, KC_RALT, KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT), 
 
     [_ADJUST] = LAYOUT_all(
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_PARENS] = LAYOUT_all(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-    _______, _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, KC_GRV , KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, _______, _______, _______, _______, _______, _______, _______, _______, 
     _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ), 
