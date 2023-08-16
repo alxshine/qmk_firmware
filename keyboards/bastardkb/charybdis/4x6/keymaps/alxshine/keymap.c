0d9b20f6eb00a54898a9466b4ccf6393d5df1f1a
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-08-16 14:34:17 +0200

    more layer changes, move numpad to regular numbers

--- keyboards/bastardkb/charybdis/4x6/keymaps/alxshine/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/alxshine/keymap.c
@@ -68,7 +68,7 @@ LCTL_T(KC_ESC),   KC_A,    KC_O,    NUMPAD,    ARROW,   KC_I,     KC_D,    PAREN
        KC_LSFT,   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
   LT(LAYER_LOWER, KC_BSPC), LGUI_T(KC_SPC), LALT_T(KC_BTN1),     RAISE,  KC_ENT,
-                                 KC_TAB,  TT(LAYER_POINTER),     KC_BTN1
+                                 KC_TAB,  TT(LAYER_POINTER),     TG(LAYER_POINTER)
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -76,9 +76,9 @@ LCTL_T(KC_ESC),   KC_A,    KC_O,    NUMPAD,    ARROW,   KC_I,     KC_D,    PAREN
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC,   KC_HASH, KC_ASTR,   KC_P9, G(S(A(KC_C))), KC_VOLU,
+       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC, KC_HASH, KC_ASTR, KC_PERC, G(S(A(KC_C))), KC_VOLU,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    S(KC_EQL),   KC_EQL, KC_EQL, KC_PMNS, KC_BSLS, KC_MUTE,
+       KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    S(KC_EQL),   KC_EQL, KC_EQL, KC_MINS, KC_BSLS, KC_MUTE,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,  QK_BOOT,    KC_PAST,   KC_BSPC, KC_DEL, KC_MNXT, G(S(A(KC_R))), KC_VOLD,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
@@ -117,15 +117,15 @@ LCTL_T(KC_ESC),   KC_A,    KC_O,    NUMPAD,    ARROW,   KC_I,     KC_D,    PAREN
   ),
   [LAYER_NUMPAD] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6, KC_F7, KC_PSLS, KC_PAST,  KC_F10,  KC_F11,
+        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6, KC_F7, KC_SLSH, KC_ASTR,  KC_F10,  KC_F11,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MNXT, KC_LSFT, KC_LSFT, KC_LSFT, KC_LSFT, XXXXXXX,    XXXXXXX, KC_P1, KC_P2,   KC_P3, KC_PMNS, KC_VOLU,
+       KC_MNXT, KC_LSFT, KC_LSFT, KC_LSFT, KC_LSFT, XXXXXXX,    XXXXXXX,  KC_1,  KC_2,  KC_3, KC_MINS, KC_VOLU,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPLY, KC_LCTL, KC_LCTL, _______, KC_LCTL, XXXXXXX,    XXXXXXX, KC_P4, KC_P5,   KC_P6, KC_PPLS, KC_BSLS,
+       KC_MPLY, KC_LCTL, KC_LCTL, _______, KC_LCTL, XXXXXXX,    XXXXXXX,  KC_4,  KC_5,  KC_6, KC_PLUS, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPRV, KC_LALT, KC_LALT, KC_LALT,  KC_LALT, XXXXXXX,   XXXXXXX, KC_P7, KC_P8,   KC_P9, KC_DOT, KC_COMM,
+       KC_MPRV, KC_LALT, KC_LALT, KC_LALT,  KC_LALT, XXXXXXX,   KC_EQL,  KC_7,  KC_8,  KC_9, KC_DOT, KC_COMM,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, KC_LGUI, XXXXXXX,    KC_NUM, KC_P0,
+                                  _______, KC_LGUI, XXXXXXX,    KC_NUM, KC_0,
                                            _______, _______,    KC_ENT
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
@@ -175,14 +175,14 @@ LCTL_T(KC_ESC),   KC_A,    KC_O,    NUMPAD,    ARROW,   KC_I,     KC_D,    PAREN
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, KC_LCTL, KC_LCTL, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, _______, DRGSCRL, SNIPING,  EE_CLR, XXXXXXX,    XXXXXXX,  KC_BTN1, DRGSCRL, KC_BTN2, SNIPING, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1,
-                                           KC_BTN2, DEFAULT,    KC_BTN2
+                                           KC_BTN2, DEFAULT,    KC_TRNS
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
